d71fcb6f6ede2bf7f4fb473b3d5000ebbbb23007
christian <christian1__@hotmail.com>
2024-03-04 20:28:38 -0500

    add fits com

---
+++ sanity/lib/client.ts
@@ -0,0 +1,11 @@
+import { createClient } from "next-sanity"
+
+import { apiVersion, dataset, projectId, useCdn } from "../env"
+
+export const client = createClient({
+  apiVersion,
+  dataset,
+  projectId,
+  useCdn,
+  token:"skn6WY32FyFjGUdNBVHn5oXKxnLCbFbWoYSWdWC7bVsIDpY1yPUzsTNpj3JiQZImQcJIaAbeRhMLjVkZATSZEyY8qmmcJOecAlOBg0r72NSvEgqtwDr2nqFL1bDMKCl30XdU3g5ChdRO2WSjDYk4xlC5hsXA0XOGR67XqaQtv0ttZ22crvBK"
+})
