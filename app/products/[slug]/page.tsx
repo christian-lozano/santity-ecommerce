ac0415a2e70a4001d40ac3231b0977b3c7e13545
christian lozano <christian-lozano>
2024-03-13 19:00:54 -0500

    add nav ver producto

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -2,17 +2,19 @@ import { client } from "@/sanity/lib/client"
 import { groq } from "next-sanity"
 
 import { SanityProduct } from "@/config/inventory"
+import { SiteHeader } from "@/components/Header/site-header"
 import { ProductGallery } from "@/components/product-gallery"
 import { ProductInfo } from "@/components/product-info"
 
 interface Props {
-  params:{
-    slug:string
+  params: {
+    slug: string
   }
 }
 
-export default async function Page({params} : Props) {
-  const product = await client.fetch<SanityProduct>(groq `*[_type == "product" && slug.current == "${params.slug}"][0] {
+export default async function Page({ params }: Props) {
+  const product =
+    await client.fetch<SanityProduct>(groq`*[_type == "product" && slug.current == "${params.slug}"][0] {
     _id,
     _createAt,
     "id":_id,
@@ -30,16 +32,19 @@ export default async function Page({params} : Props) {
 
   }`)
   return (
-    <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
-      <div className="mx-auto max-w-2xl lg:max-w-none">
-        {/* Product */}
-        <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
-          {/* Product gallery */}
-          <ProductGallery product={product}/>
-          {/* Product info */}
-          <ProductInfo product={product}/>
+    <>
+      <SiteHeader />
+      <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:max-w-none">
+          {/* Product */}
+          <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
+            {/* Product gallery */}
+            <ProductGallery product={product} />
+            {/* Product info */}
+            <ProductInfo product={product} />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
--- components/hombre-mujer/semi-filtro-home.jsx
+++ components/hombre-mujer/semi-filtro-home.jsx
@@ -17,9 +17,9 @@ export default function SemiFiltroHome({ dataSemifiltroHome }) {
       </picture>
       <div className="flex h-full  items-center justify-center gap-1 xl:items-start xl:gap-x-8  ">
         {dataSemifiltroHome.categorias.map((el) => (
-          <div className="mt-3 flex h-full w-full flex-col justify-start xl:mt-0">
+          <div className="mt-3 flex h-full w-full flex-col justify-start xl:mt-0 ">
             <img src={el.img} alt="" width={350} height={350} className="" />
-            <div className="flex flex-col  justify-center xl:block">
+            <div className="flex flex-col  justify-center xl:items-center">
               <h3 className="text-bold py-2 text-center  font-extrabold  capitalize xl:text-2xl">
                 {el.title}
               </h3>
