9609ad111a01b03b1b059b58aa4962fbcd61971e
christian lozano <christian-lozano>
2024-03-15 15:55:25 -0500

    remove mr logo mobil

--- components/Header/Nav/nav-top.tsx
+++ components/Header/Nav/nav-top.tsx
@@ -72,20 +72,37 @@ export default function NavTop({ children }: Props) {
 
             {/* burger menu */}
             <button className="xl:hidden " onClick={() => setOpen(!open)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
+              {open ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              )}
             </button>
             <MultilevelSidebar
               open={open}
--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -5,7 +5,7 @@ import { Icons } from "@/components/icons"
 
 export function Logo() {
   return (
-    <div className=" mr-8 flex gap-6 md:gap-10">
+    <div className="  flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-20 w-20 fill-black dark:fill-white" />
       </Link>
