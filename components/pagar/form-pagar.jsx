44c627ec23a7a96d596ec92c4d60f6558713bbb5
christian lozano <christian-lozano>
2024-04-03 17:58:18 -0500

    add variables production

--- components/pagar/form-pagar.jsx
+++ components/pagar/form-pagar.jsx
@@ -138,6 +138,7 @@ export default function FormPagar() {
 
         // setLoadingMercadoPago(false)
         // router.refresh()
+        //test
         // alert(data.msg)
       }
       if (res.status === 401) {
